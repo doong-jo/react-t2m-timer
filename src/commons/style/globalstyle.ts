92ea21f04338168df7f6f91f4156040b1966a1aa
doong-jo <sdong001@gmail.com>
2020-01-26 18:10:02 +0900

    refactor: remove no used fonts

--- src/commons/style/globalstyle.ts
+++ src/commons/style/globalstyle.ts
@@ -2,27 +2,21 @@ import { createGlobalStyle } from 'styled-components';
 import { theme } from 'styled-tools';
 
 const GlobalStyles = createGlobalStyle`
-  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
   @import url('https://fonts.googleapis.com/css?family=Gothic+A1|Noto+Sans+KR&display=swap');
-  @font-face {
-    font-family: 'S-CoreDream-8Heavy';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-8Heavy.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
   * {
     margin: 0;
-    font-family: 'Spoqa Han Sans', 'Noto Sans KR', 'Gothic A1', sans-serif;
+    font-family: 'Noto Sans KR', 'Gothic A1', sans-serif;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color:inherit;
   }
   html {
     font-size: 10px;
   }
   body {
     height: 100%;
-    padding: 0rem 5rem;
+    padding: 1rem 5rem;
   }
   a, button {
     text-decoration: none;
