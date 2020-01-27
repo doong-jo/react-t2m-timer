2ab46d5f5259cbbf50cdf3c04b8cf86453ff0576
doong-jo <sdong001@gmail.com>
2020-01-27 14:18:40 +0000

    Deploying to gh-pages from  - 1629be1f8b02c7f822f9f1b771e823b7109377c4 🚀

---
+++ precache-manifest.7ecf69b3804d84a83d54e3384237cb58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c7f5c8076036fe2b2e25949a9c9d66",
+    "url": "/react-t2m-timer/index.html"
+  },
+  {
+    "revision": "6fc346fd0b5cf0958966",
+    "url": "/react-t2m-timer/static/css/main.d0129bc3.chunk.css"
+  },
+  {
+    "revision": "f62b6ad21ca7a4f4a6cd",
+    "url": "/react-t2m-timer/static/js/2.da85784c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-t2m-timer/static/js/2.da85784c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fc346fd0b5cf0958966",
+    "url": "/react-t2m-timer/static/js/main.69386fe4.chunk.js"
+  },
+  {
+    "revision": "aebc6603aae0936800f1",
+    "url": "/react-t2m-timer/static/js/runtime-main.becc1b0c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45d14cfd0a556f68e70bb2b22616d432.js"
+  "/react-t2m-timer/precache-manifest.7ecf69b3804d84a83d54e3384237cb58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-t2m-timer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
